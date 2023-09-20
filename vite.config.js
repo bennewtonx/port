a17926290ff13a60a13d0137ff97344f0cd16280
Ben Newton <ben.newton112@gmail.com>
2023-09-20 11:06:56 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Make sure to add 'react-icons' to external to prevent Vite from bundling it
+      external: ['react-icons'],
+    },
+  },
 })
